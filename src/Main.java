7cf04d3de33dd358442aac3bfca37c3a0c456d58
NataA <nyabramova@gmail.com>
2023-06-07 22:06:07 +0200

    Bonus calculation

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,18 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        int initialBalance = 100; // Начальный баланс
+        int topup = 1200; // Сумма пополнения
 
+        boolean valid = topup >= 1000; // Условие для начисления бонуса
+
+        int percent = valid ? 1 : 0;
+
+        int bonus = topup * percent / 100;
+
+        System.out.println("Количество бонусных рублей:");
+        System.out.println(bonus);
+        System.out.println("Итоговый счет:");
+        System.out.println(initialBalance + topup + bonus);
     }
 }
